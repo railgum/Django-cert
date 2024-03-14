f18e41f879bc3457f19317f3fb55ea1f6453c074
Rail <railbolo@gmail.com>
2024-03-14 06:22:38 +0400

    наладка

--- cert/settings.py
+++ cert/settings.py
@@ -146,7 +146,7 @@ STATIC_URL = 'static/'
 #     os.path.join(BASE_DIR, 'static'),
 # ]
 STATIC_ROOT = BASE_DIR / 'static'
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 # LOGOUT_REDIRECT_URL = '/'
 # Default primary key field type
